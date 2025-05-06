96dd9ab2ced144506d848a2a37f40c2eff8a4432
baiel <raimkulovbaiel2@gmail.com>
2025-05-06 18:21:33 +0600

    added

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,19 +1,12 @@
 import React from "react";
-import "./style.css"; 
+import "./style.css";
 import footerLogo from "../../assets/img/footer/img1.png";
 
 const MobileFooter: React.FC = () => {
   return (
     <div className="mobile-footer">
-      <img
-        src={footerLogo}
-        alt=" Logo"
-        className="footer-logo"
-      />
-      <p className="footer-text">
-        This site was made on Tilda — a website builder that helps to create a
-        website without any code
-      </p>
+      <img src={footerLogo} alt=" Logo" className="footer-logo" />
+      <p className="footer-text"></p>
       <button className="footer-button">Create a website</button>
       <div className="footer-links">
         <a href="#">How to remove this block?</a>
